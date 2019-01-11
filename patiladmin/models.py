c8bf636d9471086d2d2a7e2d53f5fa7145da6360
Indrajeet Javeri <indrajeet.javeri3@gmail.com>
2019-01-11 11:12:37 +0530

    Date Field Added to models

--- patiladmin/models.py
+++ patiladmin/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 class client(models.Model):
     first_name = models.CharField(max_length=30)
@@ -11,8 +11,8 @@ class client(models.Model):
     city = models.CharField(max_length=20)
     state = models.CharField(max_length=20)
     zipcode = models.BigIntegerField()
-    dateofvisit = models.DateField()
-    
+    dateofvisit = models.DateField(default=timezone.now)
+
     def __str__(self):
         return (self.first_name + " " + self.last_name)
     
