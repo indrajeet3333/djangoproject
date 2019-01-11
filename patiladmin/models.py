bb4a3297489631b03a6157629529458b8fb94625
Indrajeet Javeri <indrajeet.javeri3@gmail.com>
2019-01-11 10:13:58 +0530

    Added Models

--- patiladmin/models.py
+++ patiladmin/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    contact = models.BigIntegerField()
+    email = models.CharField(max_length=50)
+    pVisit = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20)
+    zipcode = models.BigIntegerField()
+    
+    def __str__(self):
+        return (self.first_name + " " + self.last_name)
+    
